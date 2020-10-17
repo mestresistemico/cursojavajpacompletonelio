e4e808e3dc0da8f378ac85f4b07af7ae61d40f84
Aurélio L. Andrade <aurelio@sistemico.com.br>
2020-10-17 20:41:30 -0300

    Project created

---
+++ src/main/java/com/mestresistemico/cursojavajpacompletonelio/CursojavajpacompletonelioApplication.java
@@ -0,0 +1,13 @@
+package com.mestresistemico.cursojavajpacompletonelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavajpacompletonelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojavajpacompletonelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mestresistemico/cursojavajpacompletonelio/CursojavajpacompletonelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mestresistemico.cursojavajpacompletonelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojavajpacompletonelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
