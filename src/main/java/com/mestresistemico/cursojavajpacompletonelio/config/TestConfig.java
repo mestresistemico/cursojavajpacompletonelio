6d776b2bc76e16ce837ef00fd14806d596421f8f
Aurélio L. Andrade <aurelio@sistemico.com.br>
2020-10-19 15:20:11 -0300

    OrderStatus enum

--- src/main/java/com/mestresistemico/cursojavajpacompletonelio/config/TestConfig.java
+++ src/main/java/com/mestresistemico/cursojavajpacompletonelio/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.mestresistemico.cursojavajpacompletonelio.domain.Order;
 import com.mestresistemico.cursojavajpacompletonelio.domain.User;
+import com.mestresistemico.cursojavajpacompletonelio.domain.enums.OrderStatus;
 import com.mestresistemico.cursojavajpacompletonelio.repositories.OrderRepository;
 import com.mestresistemico.cursojavajpacompletonelio.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
